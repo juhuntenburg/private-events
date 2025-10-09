402cc1d7b0518c6f262472b709410f2c44d14850
juhuntenburg <ju.huntenburg@gmail.com>
2025-10-09 18:51:21 +0200

    Making users event hosts

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_10_09_163303) do
+ActiveRecord::Schema[8.0].define(version: 2025_10_09_164214) do
   create_table "events", force: :cascade do |t|
     t.string "name"
     t.string "location"
     t.date "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "host_id", null: false
+    t.index ["host_id"], name: "index_events_on_host_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -31,4 +33,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_10_09_163303) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "events", "users", column: "host_id"
 end
