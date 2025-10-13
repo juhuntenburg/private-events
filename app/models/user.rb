797d3d9e29ff28613e8640417d3d27a0f2b049d4
juhuntenburg <ju.huntenburg@gmail.com>
2025-10-13 11:48:15 +0200

    User sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :hosted_events, foreign_key: "host_id", class_name: "Event"
+  validates :name, presence: true
 end
