ff7f830823eb26d38250603941e825f98e7dbd82
juhuntenburg <ju.huntenburg@gmail.com>
2025-10-14 18:25:31 +0200

    Creating events

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get "users/index"
+  get "users/show"
   devise_for :users
-  resources :events, only: [:index]
+  resources :events, only: [:index, :show, :new, :create]
+  resources :users, only: [:index, :show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
