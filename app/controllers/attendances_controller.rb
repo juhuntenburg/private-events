4d967217106151df7bfcf94954afad096d0cfad5
juhuntenburg <ju.huntenburg@gmail.com>
2025-10-15 16:36:09 +0200

    Solve attending attended event

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -2,22 +2,23 @@ class AttendancesController < ApplicationController
   before_action :authenticate_user!, only: [:create]
 
   def create
-    @attendance = Attendance.new(
-      attended_event_id: event_params[:attended_event_id],
-      attendee_id: current_user.id
-    )
-    if @attendance.save
-      redirect_to user_path(current_user), notice: "Attendance confirmed."
+    if Attendance.exists?(attendance_params)
+      redirect_to user_path(current_user), notice: "You already registered for this event."
     else
-      redirect_to events_path, notice: "Attendance not registered."
+      @attendance = Attendance.new(attendance_params)
+      if @attendance.save
+        redirect_to user_path(current_user), notice: "Attendance confirmed."
+      else
+        redirect_to events_path, notice: "Attendance not registered."
+      end
     end
 
   end
 
   private
 
-  def event_params
-    params.require(:attendance).permit(:attended_event_id)
+  def attendance_params
+    params.require(:attendance).permit(:attended_event_id, :attendee_id)
   end
 
 end
